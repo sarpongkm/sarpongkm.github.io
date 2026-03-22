21d78cc044402c9a32881718eb81a3794b4ccdea
Kwabena Mensah Sarpong <sarpongkwabenamensah@gmail.com>
2026-03-22 18:57:29 -0400

    Add button click event to display message

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("messageBtn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", function () {
+  message.textContent = "Thanks for visiting my GitHub homepage!";
+});
